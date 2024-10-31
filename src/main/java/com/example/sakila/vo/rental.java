b1e4d96f6229814ad36770acff3bd664f5070b30
GD <GD@DESKTOP-P69J2B1>
2024-10-31 17:46:36 +0900

    vo 추가

---
+++ src/main/java/com/example/sakila/vo/rental.java
@@ -0,0 +1,16 @@
+package com.example.sakila.vo;
+
+import lombok.Data;
+
+@Data
+
+public class rental {
+	
+	private int rentalId;
+	private String rentalDate;
+	private int inventoryId;
+	private int customerId;
+	private String returnDate;
+	private int staffId;
+	private String lastUpdate;
+}
---
+++ src/main/java/com/example/sakila/vo/store.java
@@ -0,0 +1,12 @@
+package com.example.sakila.vo;
+
+import lombok.Data;
+
+@Data
+public class store {
+	private int StoreId;
+	private int managerStaffId;
+	private int adressId;
+	private String lastUpdate;
+	
+}
